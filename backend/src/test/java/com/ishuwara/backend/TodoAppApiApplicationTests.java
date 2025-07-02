976a459cb35d4a6c75588335c6982d2b2c96dece
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2025-07-02 23:05:40 +0530

    Spring boot initiate

---
+++ backend/src/test/java/com/ishuwara/backend/TodoAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishuwara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
